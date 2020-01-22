f34f3298f1810f50f08fa14c5ea66fcf5bf94058
Ben Elo <benjamin.david.elo@gmail.com>
2020-01-22 13:52:49 +0000

    Added data generation job

---
+++ job_train_wavenet_multi_gpu.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Teach-LongJobs
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=3-08:00:00
+
+export CUDA_HOME=/opt/cuda-10.1.168_418_67
+
+export CUDNN_HOME=/opt/cuDNN-7.6.0.64_10.1
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+export PROJECT_PATH=$HOME/ClaireNet
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp_proj
+
+python copy_data.py
+
+cd wavenet
+
+python train.py --data_dir $DATASET_DIR --num_steps 150000 --silence_threshold 0 --max_checkpoints 300 --checkpoint_every 300
+
